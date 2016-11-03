e0d3451aeaa01633cf14f37e09b334e286862106
Yan Zhou <zhouyan@me.com>
2016-11-04 05:51:08 +0800

    include backend_omp

--- include/mckl/smp.hpp
+++ include/mckl/smp.hpp
@@ -34,11 +34,9 @@
 
 #include <mckl/internal/config.h>
 #include <mckl/smp/backend_base.hpp>
+#include <mckl/smp/backend_omp.hpp>
 #include <mckl/smp/backend_seq.hpp>
 #include <mckl/smp/backend_std.hpp>
-#if MCKL_HAS_OMP
-#include <mckl/smp/backend_omp.hpp>
-#endif
 #if MCKL_HAS_TBB
 #include <mckl/smp/backend_tbb.hpp>
 #endif
