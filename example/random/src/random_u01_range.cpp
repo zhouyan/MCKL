fee62c8e8cd95dc15fc6e04f381ab08ef61b80f7
Yan Zhou <zhouyan@me.com>
2016-11-07 03:18:01 +0800

    remove unused macro;

--- example/random/src/random_u01_range.cpp
+++ example/random/src/random_u01_range.cpp
@@ -29,10 +29,6 @@
 // POSSIBILITY OF SUCH DAMAGE.
 //============================================================================
 
-#define MCKL_U01_USE_FIXED_POINT 0
-
-#define MCKL_U01_USE_64BITS_DOUBLE 0
-
 #include "random_u01_range.hpp"
 
 int main(int argc, char **argv)
