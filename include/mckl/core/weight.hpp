2402c7b6d26f48ea9692f52565d9d091668c9096
Yan Zhou <zhouyan@me.com>
2016-12-23 22:25:10 +0800

    remove WeightNull

--- include/mckl/core/weight.hpp
+++ include/mckl/core/weight.hpp
@@ -51,9 +51,6 @@ class Weight
     size_type size() const { return data_.size(); }
 
     /// \brief Resize the Weight object
-    ///
-    /// \details
-    /// After resizing, if the size changed, equal weights are set
     void resize(size_type N)
     {
         if (N == size())
@@ -101,7 +98,7 @@ class Weight
     template <typename InputIter>
     void mul(InputIter first)
     {
-        for (std::size_t i = 0; i != size(); ++i, ++first)
+        for (size_type i = 0; i != size(); ++i, ++first)
             data_[i] *= *first;
         normalize(false);
     }
@@ -129,7 +126,7 @@ class Weight
     void add_log(InputIter first)
     {
         log(size(), data_.data(), data_.data());
-        for (std::size_t i = 0; i != size(); ++i)
+        for (size_type i = 0; i != size(); ++i)
             data_[i] += *first;
         normalize(true);
     }
@@ -150,22 +147,23 @@ class Weight
     template <typename RNGType>
     size_type draw(RNGType &rng) const
     {
-        return draw_(rng, data_.begin(), data_.end(), true);
+        DiscreteDistribution<size_type> dist;
+
+        return dist(rng, data_.begin(), data_.end(), true);
     }
 
     private:
     double ess_;
     Vector<double> data_;
-    DiscreteDistribution<size_type> draw_;
 
     template <typename InputIter>
-    double max_element(std::size_t n, InputIter first)
+    double max_element(size_type n, InputIter first)
     {
         using value_type =
             typename std::iterator_traits<InputIter>::value_type;
 
         value_type v = -const_inf<value_type>();
-        for (std::size_t i = 0; i != n; ++i, ++first)
+        for (size_type i = 0; i != n; ++i, ++first)
             if (v < *first)
                 v = *first;
 
@@ -178,17 +176,17 @@ class Weight
         double accw = 0;
         double essw = 0;
         const double lmax = use_log ? max_element(size(), w) : 0;
-        const std::size_t k = internal::BufferSize<double>::value;
-        const std::size_t m = size() / k;
-        const std::size_t l = size() % k;
-        for (std::size_t i = 0; i != m; ++i, w += k)
+        const size_type k = internal::BufferSize<double>::value;
+        const size_type m = size() / k;
+        const size_type l = size() % k;
+        for (size_type i = 0; i != m; ++i, w += k)
             normalize(k, w, accw, essw, use_log, lmax);
         normalize(l, w, accw, essw, use_log, lmax);
         ::mckl::mul(size(), 1 / accw, data_.data(), data_.data());
         ess_ = accw * accw / essw;
     }
 
-    void normalize(std::size_t n, double *w, double &accw, double &essw,
+    void normalize(size_type n, double *w, double &accw, double &essw,
         bool use_log, double lmax)
     {
         if (use_log) {
@@ -200,73 +198,11 @@ class Weight
         essw +=
             internal::cblas_ddot(static_cast<MCKL_BLAS_INT>(n), w, 1, w, 1);
 #else  // MCKL_HAS_BLAS
-        for (std::size_t i = 0; i != n; ++i)
+        for (size_type i = 0; i != n; ++i)
             essw += w[i] * w[i];
 #endif // MCKL_HAS_BLAS
     }
 }; // class Weight
-
-/// \brief An empty weight set class
-/// \ingroup Core
-///
-/// \details
-/// This class provides all the interfaces of Weight, while they do nothing at
-/// all and the class cost no memory space. This is primarily to be used in
-/// algorithms where weights are irrelevant or managed outside the sampler for
-/// any reason. Any attempt of using methods of this class will not result in
-/// compile time or runtime errors.
-class WeightNull
-{
-    public:
-    using size_type = std::size_t;
-
-    explicit WeightNull(size_type) {}
-
-    size_type size() const { return 0; }
-
-    void resize(size_type) {}
-
-    void reserve(size_type) {}
-
-    void shrink_to_fit() {}
-
-    double ess() const { return const_nan<double>(); }
-
-    const double *data() const { return nullptr; }
-
-    template <typename OutputIter>
-    void read(OutputIter) const
-    {
-    }
-
-    void set_equal() {}
-
-    template <typename InputIter>
-    void set(InputIter)
-    {
-    }
-
-    template <typename InputIter>
-    void mul(InputIter)
-    {
-    }
-
-    template <typename InputIter>
-    void set_log(InputIter)
-    {
-    }
-
-    template <typename InputIter>
-    void add_log(InputIter)
-    {
-    }
-
-    template <typename RNGType>
-    size_type draw(RNGType &) const
-    {
-        return 0;
-    }
-}; // class WeightNull
 
 /// \brief Particle::weight_type trait
 /// \ingroup Traits
