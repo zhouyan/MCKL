e96c1a9e593f1b47be4ada92bfeb79249cdf5100
Yan Zhou <zhouyan@me.com>
2017-06-08 16:47:55 +0800

    use tbb by default

--- include/mckl/core/memory.hpp
+++ include/mckl/core/memory.hpp
@@ -72,10 +72,10 @@
 /// \brief Default allocation type
 /// \ingroup Config
 #ifndef MCKL_MEMORY_TYPE
-#if MCKL_USE_JEMALLOC
-#define MCKL_MEMORY_TYPE ::mckl::MemoryJEM
-#elif MCKL_USE_TBB_MALLOC
+#if MCKL_USE_TBB_MALLOC
 #define MCKL_MEMORY_TYPE ::mckl::MemoryTBB
+#elif MCKL_USE_JEMALLOC
+#define MCKL_MEMORY_TYPE ::mckl::MemoryJEM
 #elif MCKL_HAS_POSIX
 #define MCKL_MEMORY_TYPE ::mckl::MemorySYS
 #else
